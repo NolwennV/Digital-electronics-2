8e86988465ed77c2950a29e999db66b9269d65fc
Nolwenn Violette <lab661@PC-444.urel.feec.vutbr.cz>
2019-10-10 15:23:29 +0200

    added gpio.h

---
+++ projects/library/inc/gpio.h
@@ -0,0 +1,11 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+void GPIO_config_output (volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_config_input_nopull (volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_config_input_pullup (volatile uint8_t *reg_name, uint8_t pin_num);
+void GPIO_write (volatile uint8_t *reg_name, uint8_t pin_num, uint8_t pin_val);
+void GPIO_toggle (volatile uint8_t *reg_name, uint8_t pin_num);
+uint8_t GPIO_read (volatile uint8_t *reg_name, uint8_t pin_num);
+
+#endif /* GPIO_H */

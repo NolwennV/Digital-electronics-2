d91b788baffedeb18e4cc1693715d3f352f18c19
Nolwenn Violette <lab661@PC-444.urel.feec.vutbr.cz>
2019-10-17 16:46:59 +0200

    Added lab4 : interrupts

--- projects/library/inc/gpio.h
+++ projects/library/inc/gpio.h
@@ -3,6 +3,9 @@
 
 #include  <avr/io.h>
 
+#define PIN_LOW 0
+#define PIN_HIGH 1
+
 void GPIO_config_output (volatile uint8_t *reg_name, uint8_t pin_num);
 void GPIO_config_input_nopull (volatile uint8_t *reg_name, uint8_t pin_num);
 void GPIO_config_input_pullup (volatile uint8_t *reg_name, uint8_t pin_num);
--- projects/library/src/gpio.c
+++ projects/library/src/gpio.c
@@ -22,7 +22,7 @@ void GPIO_config_input_pullup (volatile uint8_t *reg_name, uint8_t pin_num){
 
 void GPIO_write (volatile uint8_t *reg_name, uint8_t pin_num, uint8_t pin_val){
     
-     if (pin_val == 0)
+     if (pin_val == PIN_LOW)
         *reg_name &=~ _BV(pin_num);
     else
         *reg_name |= _BV(pin_num);
